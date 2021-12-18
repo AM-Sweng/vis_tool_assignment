35f3631a12424cd1499d1f6d4c2a23bd3b6171cb
A-Mulvihill <mulvihad@tcd.ie>
2021-12-18 15:10:50 +0000

    Got PyGithub working, with a small example query put into a JSON object

--- initialscript.py
+++ initialscript.py
@@ -1,2 +1,15 @@
-# Just a hello world program to add to the github repo at the start
-print("Hello World!")
+from github import Github
+import json
+
+gitObj = Github()
+
+user = gitObj.get_user("AM-Sweng")
+
+dict = {
+    'user': user.login,
+    'fullname': user.name,
+    'location': user.location,
+    'company': user.company
+}
+
+print("The dictionary is " + json.dumps(dict))
