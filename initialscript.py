1f28e8a58144e7a2cb4f0d6fb6353968f2a0f206
A-Mulvihill <mulvihad@tcd.ie>
2021-12-19 12:54:39 +0000

    Data pulled from the API is now stored in a json file. Username and full name are not stored to keep the data anonymous. Added json files to gitignore

--- initialscript.py
+++ initialscript.py
@@ -5,11 +5,19 @@ gitObj = Github()
 
 user = gitObj.get_user("AM-Sweng")
 
-dict = {
-    'user': user.login,
-    'fullname': user.name,
-    'location': user.location,
-    'company': user.company
+usr_list = []
+usr_data = {
+    'public_repos': user.public_repos,
+    'contribs': user.contributions
 }
+usr_list.append(usr_data)
+followers = user.get_followers()
+for fl in followers:
+    usr_data = {
+        'public_repos': fl.public_repos,
+        'contribs': fl.contributions
+    }
+    usr_list.append(usr_data)
 
-print("The dictionary is " + json.dumps(dict))
+with open('data.json', 'w') as file:
+    file.write(json.dumps(usr_list))
