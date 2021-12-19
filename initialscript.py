e1010e95d9cb34b4c4fef68f2b46cedb2fcf7a29
A-Mulvihill <mulvihad@tcd.ie>
2021-12-19 15:25:29 +0000

    Changed initialscript to write to a csv file since only two value types are being used for a scatter plot. Changed index to use the column names used in the data csv that initialscript would produce. Added csv files to gitignore

--- initialscript.py
+++ initialscript.py
@@ -1,23 +1,12 @@
 from github import Github
-import json
 
 gitObj = Github()
 
 user = gitObj.get_user("AM-Sweng")
 
-usr_list = []
-usr_data = {
-    'public_repos': user.public_repos,
-    'contribs': user.contributions
-}
-usr_list.append(usr_data)
-followers = user.get_followers()
-for fl in followers:
-    usr_data = {
-        'public_repos': fl.public_repos,
-        'contribs': fl.contributions
-    }
-    usr_list.append(usr_data)
-
-with open('data.json', 'w') as file:
-    file.write(json.dumps(usr_list))
+with open('data.csv', 'w') as file:
+    file.write('public_repos,contribs\n')
+    file.write(user.public_repos + ',' + user.contributions + '\n')
+    followers = user.get_followers()
+    for fl in followers:
+        file.write(fl.public_repos + ',' + fl.contributions + '\n')
