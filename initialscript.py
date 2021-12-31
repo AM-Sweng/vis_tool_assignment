8173c630d5b5b60d0496fe0dbc106d3d001155b2
A-Mulvihill <mulvihad@tcd.ie>
2021-12-31 12:16:08 +0000

    Changed whats being visualised since many people lack contributions. Changed to visualising average number of commits per day vs number of repos commited to. Currently only focuses on users' own repos.

--- initialscript.py
+++ initialscript.py
@@ -1,4 +1,6 @@
+from os import write
 from github import Github
+from datetime import date
 import sys
 
 gitToken = sys.argv[1]
@@ -7,11 +9,46 @@ pull_user = sys.argv[2]
 gitObj = Github(gitToken)
 user = gitObj.get_user(pull_user)
 
-with open('data.csv', 'w') as file:
-    file.write('public_repos,contribs\n')
-    if (user.public_repos != None and user.contributions != None):
-        file.write(str(user.public_repos) + ',' + str(user.contributions) + '\n')
+# Get the average number of commits per day of this user in their repos, and the number of repos involved
+repos = user.get_repos()
+repo_count = repos.totalCount
+if repo_count != 0:
+    commit_count = 0
+    commit_times = []
+    for rp in repos:
+        commits = rp.get_commits()
+        for cm in commits:
+            if cm.author != None:
+                if cm.author.login == user.login:
+                    commit_count = commit_count + 1
+                    commit_times.append(cm.commit.author.date.date()) # only need the date since we are just counting the number of commits
+    commit_times.sort()
+    delta = commit_times[-1] - commit_times[0]
+    avrg_commits_a_day = commit_count / delta.days
+else:
+    avrg_commits_a_day = 0
+
+# Write the results to a csv file and find the same data on the users followers
+with open("data.csv", "w") as file:
+    file.write("repo_count,commits_a_day\n")
+    file.write(str(repo_count) + "," + str(avrg_commits_a_day) + "\n")
     followers = user.get_followers()
     for fl in followers:
-        if (fl.public_repos != None and fl.contributions != None):
-            file.write(str(fl.public_repos) + ',' + str(fl.contributions) + '\n')
+        repos = fl.get_repos()
+        repo_count = repos.totalCount
+        if repo_count != 0:
+            commit_count = 0
+            commit_times = []
+            for rp in repos:
+                commits = rp.get_commits()
+                for cm in commits:
+                    if cm.author != None:
+                        if cm.author.login == fl.login:
+                            commit_count = commit_count + 1
+                            commit_times.append(cm.commit.author.date.date()) # only need the date since we are just counting the number of commits
+            commit_times.sort()
+            delta = commit_times[-1] - commit_times[0]
+            avrg_commits_a_day = commit_count / delta.days
+        else:
+            avrg_commits_a_day = 0
+        file.write(str(repo_count) + "," + str(avrg_commits_a_day) + "\n")
