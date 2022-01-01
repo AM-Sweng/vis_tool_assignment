80888a9372fb94dd5f99cdb617994c703fe7a226
A-Mulvihill <mulvihad@tcd.ie>
2022-01-01 14:37:36 +0000

    Added a constraint to count average number of commits over the last 3 months or 90 days, aswell as the number of repos commited to.

--- initialscript.py
+++ initialscript.py
@@ -1,6 +1,6 @@
 from os import write
 from github import Github
-from datetime import date
+from datetime import datetime, timedelta
 import sys
 
 gitToken = sys.argv[1]
@@ -9,24 +9,25 @@ pull_user = sys.argv[2]
 gitObj = Github(gitToken)
 user = gitObj.get_user(pull_user)
 
-# Get the average number of commits per day of this user in their repos, and the number of repos involved
+# Get the average number of commits per day of this user in their repos over the last 3 months, and the number of repos commited to in the last three months
 repos = user.get_repos()
 repo_count = repos.totalCount
+avrg_commits_a_day = 0
 if repo_count != 0:
     commit_count = 0
-    commit_times = []
     for rp in repos:
+        used_within_last_3_months = False # Assume this repo was not commited to by the user in the last 3 months
         commits = rp.get_commits()
         for cm in commits:
             if cm.author != None:
                 if cm.author.login == user.login:
-                    commit_count = commit_count + 1
-                    commit_times.append(cm.commit.author.date.date()) # only need the date since we are just counting the number of commits
-    commit_times.sort()
-    delta = commit_times[-1] - commit_times[0]
-    avrg_commits_a_day = commit_count / delta.days
-else:
-    avrg_commits_a_day = 0
+                    if cm.commit.author.date >= datetime.today() - timedelta(days=90): # Check that the commit was within the last 3 months (roughly 90 days)
+                        commit_count = commit_count + 1
+                        used_within_last_3_months = True
+        if not used_within_last_3_months:
+            repo_count = repo_count - 1
+    avrg_commits_a_day = commit_count / 90
+    
 
 # Write the results to a csv file and find the same data on the users followers
 with open("data.csv", "w") as file:
@@ -36,19 +37,19 @@ with open("data.csv", "w") as file:
     for fl in followers:
         repos = fl.get_repos()
         repo_count = repos.totalCount
+        avrg_commits_a_day = 0
         if repo_count != 0:
             commit_count = 0
-            commit_times = []
             for rp in repos:
+                used_within_last_3_months = False
                 commits = rp.get_commits()
                 for cm in commits:
                     if cm.author != None:
                         if cm.author.login == fl.login:
-                            commit_count = commit_count + 1
-                            commit_times.append(cm.commit.author.date.date()) # only need the date since we are just counting the number of commits
-            commit_times.sort()
-            delta = commit_times[-1] - commit_times[0]
-            avrg_commits_a_day = commit_count / delta.days
-        else:
-            avrg_commits_a_day = 0
+                            if cm.commit.author.date >= datetime.today() - timedelta(days=90):
+                                commit_count = commit_count + 1
+                                used_within_last_3_months = True
+                if not used_within_last_3_months:
+                    repo_count = repo_count - 1
+            avrg_commits_a_day = commit_count / 90
         file.write(str(repo_count) + "," + str(avrg_commits_a_day) + "\n")
